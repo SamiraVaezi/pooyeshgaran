1ccb95161e0c75d32722fc4a4bb873f8abb8a410
Samira Vaezi <samira.vaezi@grandcentrix.net>
2020-08-27 15:52:47 +0200

    Added a textView and set a text

--- app/src/main/java/com/angels/pooyeshgaran/MainActivity.kt
+++ app/src/main/java/com/angels/pooyeshgaran/MainActivity.kt
@@ -2,10 +2,19 @@ package com.angels.pooyeshgaran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val textView = findViewById<TextView>(R.id.text)
+        textView.text = "Hello Angels :D"
     }
 }
