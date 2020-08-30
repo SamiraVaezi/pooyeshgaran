3b052ff6d409c65568782b524de445fe51255057
Samira Vaezi <samira.vaezi@grandcentrix.net>
2020-08-30 15:31:35 +0200

    updated main activity to access views and set value

--- app/src/main/java/com/angels/pooyeshgaran/MainActivity.kt
+++ app/src/main/java/com/angels/pooyeshgaran/MainActivity.kt
@@ -2,10 +2,21 @@ package com.angels.pooyeshgaran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.angels.pooyeshgaran.databinding.ActivityMainBinding
+import com.angels.pooyeshgaran.databinding.UserModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainDataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val model = UserModel()
+        model.userName = "sami"
+        mainDataBinding.userModel = model
     }
 }
