c0881dc29bf7fc7066f47bd4e90a07f3781a9b17
Samira Vaezi <samira.vaezi@grandcentrix.net>
2020-09-08 12:28:09 +0200

    added another imageview

--- app/src/main/java/com/angels/pooyeshgaran/databinding/UserModel.kt
+++ app/src/main/java/com/angels/pooyeshgaran/databinding/UserModel.kt
@@ -1,7 +1,23 @@
 package com.angels.pooyeshgaran.databinding
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.angels.pooyeshgaran.R
+import com.squareup.picasso.Picasso
+
 class UserModel {
 
     lateinit var userName: String
     lateinit var userImage: String
+    lateinit var userImage2: String
+
+    @BindingAdapter("userImage")
+    fun setImageResource(view: ImageView, imageUrl: String?) {
+        val context = view.context
+        Picasso.with(context)
+            .load(imageUrl)
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_launcher_background)
+            .into(view)
+    }
 }
